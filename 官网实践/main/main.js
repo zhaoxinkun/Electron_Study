2f2286d5d3ec03066b714c6a51bb55bd12ebf392
赵小新 <zhaoxinkun1124@gmail.com>
2026-06-25 17:46:04 +0800

    chore: hd_base: init

--- 官网实践/main/main.js
+++ 官网实践/main/main.js
@@ -1,22 +1,32 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, ipcMain} from "electron"
+
 import path from "node:path"
 
+// 获取父级目录
 const appRoot = path.dirname(import.meta.dirname)
+console.log("app root:", appRoot);
+
+// 创建窗口
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        // 使用preload中间人,添加操作node能力
         webPreferences: {
-            preload: path.join(appRoot, 'preload', 'preload.cjs'),
+            preload: path.join(appRoot, "preload", "preload.cjs"),
         }
     })
 
-    win.loadFile(path.join(appRoot, "renderer", "index.html"))
-    win.webContents.openDevTools() //每次都打开开发者工具
+    // 加载的内容
+    win.loadFile(path.join(appRoot, "renderer", "index.html"));
 }
 
 // 声明周期
 app.whenReady().then(() => {
+    // 注册一个 Handler（处理器）
+    ipcMain.handle('ping', () => {
+        console.log("this is ipc preload handle 'ping' in electron")
+    })
     createWindow()
     // 当没有窗口打开时,创建新的窗口
     app.on('activate', () => {
--- 官网实践/preload/preload.cjs
+++ 官网实践/preload/preload.cjs
@@ -1,8 +1,10 @@
-const {contextBridge} = require('electron')
+const {contextBridge, ipcRenderer} = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
     // 暴露部分安全接口给渲染进程使用
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
+    // 添加ipcRenderer通信暴露给Main进程
+    ping: () => ipcRenderer.invoke('ping')
 })
